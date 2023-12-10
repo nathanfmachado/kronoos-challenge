ba58e4dc4cd09ae7f8992a6a7a02eab127fe76c5
Nathan Fabiano Machado <nathan.machado@taqtile.com.br>
2023-12-10 13:08:03 -0300

    Setting up node app

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import fs from "fs";
+import { parse } from "csv-parse"
+import { dataMapper } from "./data-mapper.js";
+
+const csvData = [];
+
+console.log("Reading and mapping CSV file...");
+const reader = fs.createReadStream("./data.csv", { encoding: 'UTF-8' })
+  .pipe(parse({ delimiter: ",", columns: true, ltrim: true }));
+
+reader.on("data", (row) => {
+    csvData.push(dataMapper(row));
+  })
+  .on("error", (error) => {
+    console.log(error.message);
+  })
+  .on("end", () => {
+    console.log("CSV file successfully processed");
+    console.log(csvData[0]);
+    console.log(csvData[300]);
+  });
+
+
+
